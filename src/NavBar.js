3b08a7c5917ef7a56966fc40360426ca6c053682
Asthio <57274664+Asthio@users.noreply.github.com>
2020-06-03 22:24:03 +0200

    some customizes

--- src/NavBar.js
+++ src/NavBar.js
@@ -2,18 +2,21 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import 'rmwc/dist/styles';
 import { Fab } from '@rmwc/fab';
+import './index.css';
 
 const NavBar = () => (
-    <nav>
-        <div></div>
-        <div><Link to="/">My Note</Link></div>
-        <div>
-            <ul>
-                <li>Your notes:</li>
-                <li><Fab label="Create new" /></li>
-            </ul>
-        </div>
-    </nav>
+    <header className="navbar">
+        <nav className="navigation">
+            <div></div>
+            <div><Link className="navbar_logo" to="/">My Note</Link></div>
+            <div className="navigation_items">
+                <ul>
+                    <li>Your notes:</li>
+                    <li><Fab label="Create new" /></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
 );
 
 export default NavBar;
